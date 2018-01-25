11e60192601f70cc00cd42f48e34a90ec62879e4
long <1538068051@qq.com>
2018-01-25 05:16:19 -0800

    爬取百度词条和主要解释信息

---
+++ example/url_manager.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*-coding=utf8-*-
+
+class UrlManager(object):
+	def __init__(self):
+		self.new_urls = set()
+		self.old_urls = set()
+
+	def add_new_url(self, url):#添加新的url
+		if url is None:
+			return
+		if url not in self.new_urls and url not in self.old_urls:
+			self.new_urls.add(url)
+
+	def add_new_urls(self,urls):#添加批量的url
+		if urls is None or len(urls) == 0:
+			return 
+		for url in urls:
+			self.add_new_url(url)
+
+	def has_new_url(self):#判断管理器中是否有这个url
+		return len(self.new_urls) != 0
+
+	def get_new_url(self):
+		new_url = self.new_urls.pop()
+		self.old_urls.add(new_url)
+		return new_url
+
